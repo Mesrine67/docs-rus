b3ebe8d87ae3ed885685d31b6a7783fd54c9d5ec
PaPi <89279156+Mesrine67@users.noreply.github.com>
2024-12-26 23:02:37 +0100

    test

--- src/components/HomepageFeatures/index.js
+++ src/components/HomepageFeatures/index.js
@@ -4,32 +4,30 @@ import styles from './styles.module.css';
 
 const FeatureList = [
   {
-    title: 'Easy to Use',
+    title: 'Facile à utiliser',
     Svg: require('@site/static/img/undraw_docusaurus_mountain.svg').default,
     description: (
       <>
-        Docusaurus was designed from the ground up to be easily installed and
-        used to get your website up and running quickly.
+        Papi-Management a été conçu dès le départ pour être facilement installé et utilisé
+        afin de mettre votre serveur fivem en ligne rapidement.
       </>
     ),
   },
   {
-    title: 'Focus on What Matters',
+    title: 'Concentrez-vous sur l’essentiel',
     Svg: require('@site/static/img/undraw_docusaurus_tree.svg').default,
     description: (
       <>
-        Docusaurus lets you focus on your docs, and we&apos;ll do the chores. Go
-        ahead and move your docs into the <code>docs</code> directory.
+        Papi-Management vous permet de vous concentrer sur votre serveur, et nous nous occupons des tâches ingrates. 
       </>
     ),
   },
   {
-    title: 'Powered by React',
+    title: 'Propulsé par React/Lua',
     Svg: require('@site/static/img/undraw_docusaurus_react.svg').default,
     description: (
       <>
-        Extend or customize your website layout by reusing React. Docusaurus can
-        be extended while reusing the same header and footer.
+        Propulsé par React/Lua.
       </>
     ),
   },
